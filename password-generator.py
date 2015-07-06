4ddbffcb91c735769dca111a426e1aa71e468d08
Benjamin <blasterbug@users.noreply.github.com>
2015-07-06 16:24:36 +0200

    start
    for the moment display every arguments

--- password-generator.py
+++ password-generator.py
@@ -4,9 +4,11 @@
 import os
 import sys
 
-def main(argv):
-    print("Finished shutdown.")
+def main( argv ):
+    for value in argv:
+        print( value )
+    print( "Finished shutdown." )
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main( sys.argv )
