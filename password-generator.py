1f4aad52b109a36e9d3fba0f68d42aed79b94d26
Benjamin <blasterbug@users.noreply.github.com>
2015-07-07 17:39:40 +0200

    cleaning unused code

--- password-generator.py
+++ password-generator.py
@@ -6,13 +6,6 @@ import sys
 import json
 import random
 
-
-def categorize_words( dct ) :
-    if any( term in dct for term in ('descriptive', 'noums') ) :
-            return list( dct['noums'], dct['descriptive'] )
-    return dct
-
-
 def main( argv ) :
     dct = json.load( open( "words.json" ) )
     print( random.choice( dct['descriptive'] ) + " " + random.choice( dct['noums'] ) )
