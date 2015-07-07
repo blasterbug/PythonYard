aa063e5711785752438697f450eecae0d21db5f5
Benjamin <blasterbug@users.noreply.github.com>
2015-07-07 17:06:17 +0200

    fun generator

--- password-generator.py
+++ password-generator.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# pick up two words and a json file and print them
 
-import os
 import sys
-
-def main( argv ):
-    for value in argv:
-        print( value )
-    print( "Finished shutdown." )
+import json
+import random
 
 
-if __name__ == "__main__":
+def categorize_words( dct ) :
+    if any( term in dct for term in ('descriptive', 'noums') ) :
+            return list( dct['noums'], dct['descriptive'] )
+    return dct
+
+
+def main( argv ) :
+    dct = json.load( open( "words.json" ) )
+    print( random.choice( dct['descriptive'] ) + " " + random.choice( dct['noums'] ) )
+
+if __name__ == "__main__" :
     main( sys.argv )
