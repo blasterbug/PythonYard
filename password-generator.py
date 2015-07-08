fdd9dde4ae7adc25f5f1ad2323ffd7acf7e45cee
Benjamin <blasterbug@users.noreply.github.com>
2015-07-08 13:59:18 +0200

    finally can generate passwords ;)

--- password-generator.py
+++ password-generator.py
@@ -6,9 +6,38 @@ import sys
 import json
 import random
 
+# macth a regular char to a 133t one
+def leetmatchor( char ) :
+    return {
+        ' ' : '',
+        'a' : '4',
+        'b' : '6',
+        'c' : '(',
+        'e' : '3',
+        'h' : 'h',
+        'i' : '!',
+        'l' : '1',
+        'o' : '0',
+        'p' : 'p',
+        'q' : 'q',
+        'r' : 'r',
+        's' : '5',
+        't' : '7',
+        'z' : '2',
+    }.get( char , char ) # default case
+
+# transforme a string to sort of 133t (leet)
+def plain2leet( string ) :
+    leetified = list( '' )
+    for char in string:
+        leetified.append( leetmatchor( char ) )
+    return ''.join( leetified )
+
 def main( argv ) :
     dct = json.load( open( "words.json" ) )
-    print( random.choice( dct['descriptive'] ) + " " + random.choice( dct['noums'] ) )
+    res = random.choice( dct['descriptive'] ) + " " + random.choice( dct['noums'] )
+    print res
+    print plain2leet( res )
 
 if __name__ == "__main__" :
     main( sys.argv )
