e0fadb1c98ff054fb0153c0454450c7e574ec930
Benjamin <blasterbug@users.noreply.github.com>
2015-07-10 11:16:59 +0200

    comments and shit

--- password-generator.py
+++ password-generator.py
@@ -10,11 +10,13 @@ import random
 from leet import plain2leet
 
 def main( argv ) :
+    # read woreds from JSON file
     dct = json.load( open( "words.json" ) )
+    # `dict` is a dictionnary where `descriptive` and `noums` lists are stored
     res = random.choice( dct['descriptive'] ) + " " + random.choice( dct['noums'] )
     print "Original string :\t" + res
     print "Password :\t\t" + plain2leet( res )
 
 if __name__ == "__main__" :
     main( sys.argv )
-    print "Consider uppercase letters to increase complexity"
+    print "Consider uppercase letters to increase strenght and a 8 digits long password"
