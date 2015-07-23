612c1adb73de8853fdee01f96c4846a2f4e42cc5
Benjamin <blasterbug@users.noreply.github.com>
2015-07-23 16:46:24 +0200

    open and store in a file

--- input2json.py
+++ input2json.py
@@ -11,8 +11,15 @@ def main( argv ) :
     # delete program name
     argv.pop( 0 )
     # put the argv list in a json list
-    jsdata = json.dumps( argv )
-    print( jsdata )
+    jsdata = json.dumps( argv, indent=True )
+    # create a tempory file
+    tmpfile = open( "tempory.json", "w")
+    # store json data in file
+    tmpfile.write( jsdata )
+    # last char
+    tmpfile.write( "\n" )
+    # close file
+    tmpfile.close()
 
 if __name__ == "__main__" :
     main( sys.argv )
