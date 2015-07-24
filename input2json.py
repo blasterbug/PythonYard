1413c36ad677b0c5d0c1217170d2b1fb61e7f439
Benjamin <blasterbug@users.noreply.github.com>
2015-07-24 11:23:52 +0200

    python3 and argument parser

--- input2json.py
+++ input2json.py
@@ -1,19 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Put into a json collection the input, the first argument is the collection
 # name. The resultat is send to the default ouput.
 
 import sys
 import json
+import argparse
 
+# parser for CLI
+parser = argparse.ArgumentParser( description='Process some data to JSON file' )
+# adding some arguments
+parser.add_argument( '-o', '--ouput', help="Give a file name to store JSON ouputs", type=open, default='tempory.json' )
+parser.add_argument( '-i', '--input', help="The input file to process", type=open )
 
-def main( argv ) :
-    # delete program name
-    argv.pop( 0 )
+def main( filename, argv ) :
     # put the argv list in a json list
     jsdata = json.dumps( argv, indent=True )
     # create a tempory file
-    tmpfile = open( "tempory.json", "w")
+    tmpfile = open( filename + ".json", "w")
     # store json data in file
     tmpfile.write( jsdata )
     # last char
@@ -22,5 +26,12 @@ def main( argv ) :
     tmpfile.close()
 
 if __name__ == "__main__" :
-    main( sys.argv )
+    args = parser.parse_args()
+    print( args )
+    try :
+        filename = sys.argv.pop( 0 )
+
+        #main( filename, sys.argv )
+    except IndexError :
+        print( "Give a filename", file=sys.stderr )
 #    print( "Finished shutdown." )
